40cf88f94ca472c25b035b3ced73738e112e2f12
ThisIsSebi <shoff439@gmail.com>
2025-03-26 08:12:04 +0100

    Changed colour of each lollipop

--- sketch.js
+++ sketch.js
@@ -6,15 +6,16 @@ function draw() {
   background(50);
 
 
-  lollipop(100,100,50);
-  lollipop(300,200,150);
+  lollipop(100,100,50,'green');
+  lollipop(300,200,150,'red');
+  lollipop(500,100,50,'blue');
 }
 
-function lollipop(x,y,diameter){
+function lollipop(x,y,diameter, colour){
   fill(0,200,255);
   rect(x-10,y,20,150);
 
-  fill(255, 0,200);
+  fill(colour);
   ellipse(x,y,diameter,diameter);
 }
 // Imagine what you want to build 
